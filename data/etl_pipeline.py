0e971a085d77cb59cf066317c8baa43bf99e7004
albokhitan <albokhitan@gmail.com>
2021-11-03 20:50:53 +0300

    Update etl_pipeline.py

--- data/etl_pipeline.py
+++ data/etl_pipeline.py
@@ -74,7 +74,7 @@ def save_data(df, database_filename):
 
        """
     engine = create_engine('sqlite:///{}'.format(database_filename))
-    df.to_sql('disaster_messages_tbl', engine, index=False)
+    df.to_sql('disaster_messages_tbl', engine, index=False, if_exists='replace')
 
 
 def main():
